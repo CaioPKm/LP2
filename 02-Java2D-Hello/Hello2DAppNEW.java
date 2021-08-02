b8dae86c4b42695029ed6382c7a2f6ea525be064
CaioPKm <74361276+CaioPKm@users.noreply.github.com>
2021-08-02 17:49:57 -0300

    Versão modificada por mim do programa(Hello2DApp.java)
    Foram adicionados:
    - Cor cinza no fundo
    - Cor vermelha no X
    - String Hello World! em azul
    - Retangulo Magenta

---
+++ 02-Java2D-Hello/Hello2DAppNEW.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Hello2DApp {
+    public static void main (String[] args) {
+        Hello2DFrame frame = new Hello2DFrame();
+    }
+}
+
+class Hello2DFrame extends JFrame {
+    public Hello2DFrame () {
+        this.addWindowListener (
+            new WindowAdapter() {
+                public void windowClosing (WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+        this.setTitle("Java2D - Hello World!");
+        this.setSize(350, 350);
+        this.getContentPane().setBackground(Color.lightGray); // changing the background's color 
+        this.setVisible(true);
+    }
+
+    public void paint (Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setPaint(Color.red);
+        int w = getWidth();
+        int h = getHeight();
+        g2d.drawLine(0,0, w,h);
+        g2d.drawLine(0,h, w,0);
+        
+        //adding a string
+        g2d.setPaint(Color.blue);
+        g2d.drawString("Hello World!", 200, 200);
+
+        //adding a fill rectangle
+        g2d.setPaint(Color.magenta);
+        g2d.fillRect(150, 80, 150, 80);
+    }
+}
