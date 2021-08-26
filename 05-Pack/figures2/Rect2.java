3e47e838b481dabd9aac114d6b65b5405f51f63c
CaioPKm <74361276+CaioPKm@users.noreply.github.com>
2021-08-26 15:01:23 -0300

    Update Rect2.java

--- 05-Pack/figures2/Rect2.java
+++ 05-Pack/figures2/Rect2.java
@@ -1,16 +1,23 @@
-package figures;
+package figures2;
 
 import java.awt.*;
 
 public class Rect2 {
     int x, y;
     int w, h;
-
-    public Rect2 (int x, int y, int w, int h) {
+    int r, g, b;
+    int r2, g2, b2;
+    public Rect2 (int x, int y, int w, int h, int r, int g, int b, int r2, int g2, int b2) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.r2 = r2;
+        this.g2 = g2;
+        this.b2 = b2;
     }
 
     public void print () {
@@ -18,11 +25,11 @@ public class Rect2 {
             this.w, this.h, this.x, this.y);
     }
 
-    public void paint (Graphics g,int r1 , int g1, int b1, int r2 , int g2, int b2) {
+    public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(new Color(r1,g1,b1));
+        g2d.setColor(new Color(this.r,this.g,this.b));
         g2d.fillRect(this.x, this.y, this.w, this.h);
-        g2d.setColor(new Color(r2,g2,b2));
+        g2d.setColor(new Color(this.r2,this.g2,this.b2));
         g2d.drawRect(this.x, this.y, this.w, this.h);
     }
 }
