efbc676c9cc606416e3c1339852830aee4c11341
CaioPKm <74361276+CaioPKm@users.noreply.github.com>
2021-09-03 17:37:35 -0300

    Create Texto.java

---
+++ 06-List/figures/Texto.java
@@ -0,0 +1,25 @@
+package figures;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Ellipse2D.Double;
+
+public class Texto {
+    int x, y;
+    String text;
+    int r, g, b;
+    public Texto (int x, int y, String text, int r, int g, int b ) {
+        this.x = x;
+        this.y = y;
+        this.text = text;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(new Color(this.r,this.g,this.b));
+        g2d.drawString(this.text,this.x,this.y);
+    }
+}
