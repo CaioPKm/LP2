25eac2fe5ae4fe0295c167de512eb07ab9046cfe
CaioPKm <74361276+CaioPKm@users.noreply.github.com>
2021-10-03 15:44:25 -0300

    Update Invisible.java

--- Projeto1-2/invisible/Invisible.java
+++ Projeto1-2/invisible/Invisible.java
@@ -5,5 +5,6 @@ import java.awt.Graphics;
 public interface Invisible {
     public boolean clicked (int x, int y); //função para dectar quando o mouse clicar na figura
     public void paint (Graphics g); //função par desenhar as figuras da tela
-
+    public abstract void focusRef (Graphics g); //função para foco das imagens 
 }
+
