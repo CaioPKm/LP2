039dd16341f1f1e89559a36938f3ca2d7ca1f965
CaioPKm <74361276+CaioPKm@users.noreply.github.com>
2021-10-04 19:42:49 -0300

    Update Figure.java

--- Projeto1-2/figures/Figure.java
+++ Projeto1-2/figures/Figure.java
@@ -1,8 +1,8 @@
 package figures;
 
-import invisible.Invisible;
+import java.awt.Graphics;
 
-public abstract class Figure implements Invisible {
+public abstract class Figure {
     public int x,y;
     public int w,h;
     public int r, g, b;
@@ -22,6 +22,9 @@ public abstract class Figure implements Invisible {
         this.y += dy;
     }
 
-    public abstract boolean clicked (int x, int y);
-
+    public abstract boolean clicked (int x, int y); //função para dectar quando o mouse clicar na figura
+    
+    public abstract void paint (Graphics g); //função par desenhar as figuras da tela
+    
+    public abstract void focusRef (Graphics g); // função para definir o foco da figura
 }
