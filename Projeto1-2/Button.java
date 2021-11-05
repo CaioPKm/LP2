971b7c2d5532150b62c729c7ca01a7561daf7f1c
CaioPKm <74361276+CaioPKm@users.noreply.github.com>
2021-11-04 21:01:51 -0300

    Button.java semana de 7/11

--- Projeto1-2/Button.java
+++ Projeto1-2/Button.java
@@ -20,7 +20,7 @@ public class Button implements IVisible {
     }
 
     public boolean clicked (int x, int y) {
-        return SPC<=x && x<=SPC+DIM && SPC+this.idx*DIM<=y && y<=SPC+this.idx*DIM+DIM;
+        return (SPC<=x && x<=SPC+DIM && SPC+this.idx*DIM<=y && y<=SPC+this.idx*DIM+DIM);
     }
 
     public void paint (Graphics g, boolean focused) {
@@ -31,7 +31,15 @@ public class Button implements IVisible {
 
         g2d.setColor(Color.BLACK);
         g2d.drawRect(SPC, SPC+this.idx*DIM, DIM, DIM);
-
+        switch(idx) {
+            case 2:
+            this.fig.y = PAD+SPC + idx*DIM +20;
+            break;
+            case 3:
+            this.fig.y = PAD+SPC + idx*DIM +20;
+            break;
+        }
         this.fig.paint(g, false);
+        
     }
 }
