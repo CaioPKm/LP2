f436c596b22407d2cec2ed71ead55f054301a460
CaioPKm <74361276+CaioPKm@users.noreply.github.com>
2021-11-14 10:33:53 -0300

    Update ArrayStack.java

--- 11-Stackable/ArrayStack.java
+++ 11-Stackable/ArrayStack.java
@@ -7,10 +7,10 @@ public class ArrayStack implements IStackable {
         return list.size();
     }
 
-    public void push (int x ) {
+    public void push(int x ) {
         list.add(0,x);
     }
-    
+
     public int pop () {
         int x = list.get(0);
         list.remove(0);
